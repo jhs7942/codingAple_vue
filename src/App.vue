cd54f9945d697ca35241c449dc210e2761c298e2
정현승 <saver7942@naver.com>
2023-04-05 23:03:53 +0900

    @click, 함수 만들어봄

--- src/App.vue
+++ src/App.vue
@@ -4,20 +4,11 @@
   </div>
   <img alt="Vue logo" src="./assets/logo.png" />
 
-  <div>
-    {{ logo }}
-    <h4>{{ proroducts[0] }}</h4>
-    <p>{{ price[0] }}만원</p>
-  </div>
-  <div>
-    {{ logo }}
-    <h4>{{ proroducts[1] }}</h4>
-    <p>{{ price[1] }}만원</p>
-  </div>
-  <div>
-    {{ logo }}
-    <h4>{{ proroducts[2] }}</h4>
-    <p>{{ price[2] }}만원</p>
+  <div v-for="(원룸, i) in proroducts" :key="원룸">
+    <h4>{{ 원룸 }}</h4>
+    <div>{{ price[i] }}만원</div>
+    <button @click="increase(i)">허위매물신고</button
+    ><span>신고수 : {{ 신고수[i] }}</span>
   </div>
 </template>
 
@@ -26,10 +17,17 @@ export default {
   name: "App",
   data() {
     return {
+      신고수: [0, 0, 0],
       메뉴들: ["Home", "Shop", "About"],
       proroducts: ["역삼동원룸", "천호동원룸", "마포구원룸"],
       price: [50, 60, 70],
     };
+  },
+  methods: {
+    increase(i) {
+      this.신고수[i] += 1;
+    },
+    함수2() {},
   },
   components: {},
 };
